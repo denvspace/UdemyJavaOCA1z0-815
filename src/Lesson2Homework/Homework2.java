0513b96c7ae4d7343873005563210dc5c00604f8
Denis Vorob'ev <mail@denv.space>
2020-02-29 12:44:13 +0300

    Второе задание выполнено

---
+++ src/Lesson2Homework/Homework2.java
@@ -0,0 +1,71 @@
+package Lesson2Homework;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        byte b1 = 0b1100;
+        byte b2 = 014;
+        byte b3 = 12;
+        byte b4 = 0xC;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 0b101_0001_0100;
+        short s2 = 02424;
+        short s3 = 1300;
+        short s4 = 0x514;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0b0;
+        int i2 = 00;
+        int i3 = 0;
+        int i4 = 0x0;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long L1 = 0b111_0101_1011_1100_1101_0001_0101L;
+        long L2 = 0726746425L;
+        long L3 = 123456789L;
+        long L4 = 0x75BCD15L;
+
+        System.out.println(L1);
+        System.out.println(L2);
+        System.out.println(L3);
+        System.out.println(L4);
+
+        float f1 = 5.16F;
+        float f2 = 5.16F;
+
+        double d1 = 2.78;
+        double d2 = 2.78D;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+        System.out.println(bool1);
+        System.out.println(bool2);
+
+        char c1 = 'D';
+        char c2 = 1567;
+        char c3 = '\u1237';
+        char c4 = '8';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
